af557a66faf479978e6e205a5681a353d100b05c
Muhammad Tarek <medojudoman@gmail.com>
2025-03-21 20:34:07 +0200

    test

--- binary search tree/binary_search_tree.cpp
+++ binary search tree/binary_search_tree.cpp
@@ -1,44 +1,7 @@
 #include <iostream>
 #include <cassert>
-
+#include "binary_search_tree.h"
 using namespace std;
-
-struct nodeType
-{
-   int     info;
-   nodeType  *left;
-   nodeType  *right;
-};
-class binarySearchTreeType
-{
-public:
-	bool isEmpty();
-	bool search(int);
-	bool searchRec(int);
-	void insert(int);
-	void remove(int);
-	void inorderTraversal();
-	void preorderTraversal();
-	void postorderTraversal();
-	int treeHeight();
-	int treeNodeCount();
-	int treeLeavesCount();
-	void clearTree();
-	binarySearchTreeType();
-	~binarySearchTreeType();
-private:
-	nodeType *root;
-    void clear(nodeType* &p);
-    void inorder(nodeType *p);
-    void preorder(nodeType *p);
-    void postorder(nodeType *p);
-    int height(nodeType *p);
-    int max(int x, int y);
-    int nodeCount(nodeType *p);
-    int leavesCount(nodeType *p);
-	void deleteFromTree(nodeType * &p);
-	bool searchRecPriv(nodeType *, int);
-};
 
 
 binarySearchTreeType::binarySearchTreeType()
---
+++ binary search tree/binary_search_tree.h
@@ -0,0 +1,45 @@
+//
+// Created by muhammad on 21/03/25.
+//
+
+#ifndef BINARY_SEARCH_TREE_H
+#define BINARY_SEARCH_TREE_H
+
+struct nodeType
+{
+    int     info;
+    nodeType  *left;
+    nodeType  *right;
+};
+class binarySearchTreeType
+{
+public:
+    bool isEmpty();
+    bool search(int);
+    bool searchRec(int);
+    void insert(int);
+    void remove(int);
+    void inorderTraversal();
+    void preorderTraversal();
+    void postorderTraversal();
+    int treeHeight();
+    int treeNodeCount();
+    int treeLeavesCount();
+    void clearTree();
+    binarySearchTreeType();
+    ~binarySearchTreeType();
+private:
+    nodeType *root;
+    void clear(nodeType* &p);
+    void inorder(nodeType *p);
+    void preorder(nodeType *p);
+    void postorder(nodeType *p);
+    int height(nodeType *p);
+    int max(int x, int y);
+    int nodeCount(nodeType *p);
+    int leavesCount(nodeType *p);
+    void deleteFromTree(nodeType * &p);
+    bool searchRecPriv(nodeType *, int);
+};
+
+#endif //BINARY_SEARCH_TREE_H
